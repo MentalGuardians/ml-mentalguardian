0d35ce052ca9cdbdc0efd58a3faa7762cef4db7e
M. Salman Al Farisy <mannaksara@gmail.com>
2023-12-06 12:05:50 +0700

    update model and data

--- content based filtering recsys/model1.py
+++ content based filtering recsys/model1.py
@@ -4,14 +4,14 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 
-data = pd.read_csv('vid_youtube.csv')
+data = pd.read_csv('content_by_metadata.csv')
 data = data.reset_index(drop=True)
 data = data.dropna(subset=['Labels', 'Likes', 'Views']).reset_index(drop=True)
 
-data['labels_and_views'] = data['Labels'] + ' ' + data['Views'].astype(str)
+data['Labels'] = data['Labels'].str.upper()
 
 vectorizer = TfidfVectorizer()
-lc_matrix = vectorizer.fit_transform(data['labels_and_views'])
+lc_matrix = vectorizer.fit_transform(data['Labels'])
 
 model = Sequential()
 model.add(Dense(64, activation='relu', input_shape=(lc_matrix.shape[1],)))
@@ -22,7 +22,9 @@ model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accur
 model.fit(lc_matrix.toarray(), np.identity(lc_matrix.shape[0]), epochs=50, batch_size=32)
 
 def get_recommendations(title):
-    df_title = data[data['Labels'] == title.upper()]
+    title = title.upper()
+    df_title = data[data['Labels'] == title]
+    
     if not df_title.empty:
         idx = df_title.index[0]
         labels_vector = lc_matrix[idx].toarray()
@@ -32,3 +34,6 @@ def get_recommendations(title):
         return recommendations_df
     else:
         return "The Labels is not found in the dataset."
+
+test = get_recommendations("love")
+print(test)
